b4ceba4ede5eb60114fbe45f833bb7ec646a317c
Subash Pandey <subash2005@gmail.com>
2024-06-04 09:21:09 -0500

    updates

--- javaClassWork/M305_JDBC/src/main/java/org/example/database/entity/Employee.java
+++ javaClassWork/M305_JDBC/src/main/java/org/example/database/entity/Employee.java
@@ -6,40 +6,41 @@ import lombok.Setter;
 
 @Setter
 @Getter
-
 @Entity
 @Table(name = "employees")
 public class Employee {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id // this is telling hibernate this column is the PK
+    @GeneratedValue(strategy = GenerationType.IDENTITY)  // this telling hibernate that the PK is auto increment
+    @Column(name = "id")
     private Integer id;
 
     @Column(name = "office_id")
     private Integer officeId;
 
-
     @Column(name = "lastname")
-    private String lastName;
+    private String lastname;
 
-    @Column(name ="firstname")
-    private String firstName;
+    @Column(name = "firstname")
+    private String firstname;
 
+    @Column(name = "extension")
     private String extension;
 
+    @Column(name = "email")
     private String email;
 
-    @Column(name ="reports_to")
+    @Column(name = "reports_to")
     private Integer reportsTo;
 
     @Column(name = "job_title")
     private String jobTitle;
 
-    @Column(name="vacation_hours")
+    @Column(name = "vacation_hours")
     private Integer vacationHours;
 
-    @Column(name= "profile_image_url")
-    private String profileImageURL;
+    @Column(name = "profile_image_url")
+    private String profileImageUrl;
 
 
 }
