f28c7fe036404695bcb2bfee12f1918fc09cfd45
Subash Pandey <subash2005@gmail.com>
2024-06-06 07:26:57 -0500

    More homework for 06/06

--- javaClassWork/M305_JDBC/src/main/java/org/example/MainCustomer.java
+++ javaClassWork/M305_JDBC/src/main/java/org/example/MainCustomer.java
@@ -8,14 +8,36 @@ import java.util.List;
 public class MainCustomer {
 
     public static void main(String[] args) {
+
+        // Now creating a customer object for inserting a customer object into the mysql
+        
+        Customer customer = new Customer();
+        customer.setCustomerName("Subash Inc");
+        customer.setContactFirstName("Subash");
+        customer.setContactLastName("Pandey");
+        customer.setCity("Kathmandu");
+        customer.setCountry("Nepal");
+        customer.setPhone("+977984143348");
+        customer.setAddressLine1("Kirtipur-1");
+        customer.setAddressLine2(null);
+        customer.setState("Bagmati");
+        customer.setPostalCode("42344");
+        customer.setSalesRepEmployeeId(1504);
+        customer.setCreditLimit(20000.00);
+
         CustomerDAO customerDAO = new CustomerDAO();
-        List<Customer> result =customerDAO.findByCustomerName("Muscle Machine Inc");
-        for (Customer customer : result) {
-            System.out.println(customer);
+        customerDAO.insert(customer);
+
+
+        List<Customer> result = customerDAO.findByCustomerName("Subash Inc");
+        for (Customer customer1 : result) {
+            System.out.println(customer1);
         }
-        List<Customer> customers = customerDAO.findByContactFirstName("First Name");
-        for (Customer customer : customers) {
-            System.out.println(customer);
+        List<Customer> customers = customerDAO.findByContactFirstName("Subash");
+        for (Customer customer1 : customers) {
+            System.out.println(customer1);
         }
+        Customer customer2 = customerDAO.findById(520);
+        System.out.println(customer2);
     }
 }
--- javaClassWork/M305_JDBC/src/main/java/org/example/database/dao/CustomerDAO.java
+++ javaClassWork/M305_JDBC/src/main/java/org/example/database/dao/CustomerDAO.java
@@ -10,6 +10,35 @@ import org.hibernate.cfg.Configuration;
 import java.util.List;
 
 public class CustomerDAO {
+
+    public Customer findById(int id) {
+        SessionFactory factory = new Configuration().configure().buildSessionFactory();
+        Session session =factory.openSession();
+        String hql = "Select c from Customer c where c.id = :id";
+        TypedQuery<Customer> query =session.createQuery(hql, Customer.class);
+        query.setParameter("id", id);
+        Customer customer = query.getSingleResult();
+        session.close();
+        return customer;
+    }
+    public void insert(Customer customer) {
+        // these 2 lines of code prepare the hibernate session for use
+        SessionFactory factory = new Configuration().configure().buildSessionFactory();
+        Session session = factory.openSession();
+
+        // begin the transaction
+        session.getTransaction().begin();
+
+        // insert the employee to the database
+        session.save(customer);
+
+        /// commit our transaction
+        session.getTransaction().commit();
+
+        // cleanup the session
+        session.close();
+    }
+
     public List<Customer> findByCustomerName(String customerName) {
         SessionFactory factory = new Configuration().configure().buildSessionFactory();
         Session session = factory.openSession();
